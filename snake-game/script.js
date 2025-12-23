a597beba6e2138f4567f3c377f4641f0523dd2d1
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-23 22:54:14 +0530

    jssnakeproject

--- snake-game/script.js
+++ snake-game/script.js
@@ -5,7 +5,20 @@ const boardWidth = 80
 const rows = Math.floor(board.clientHeight / boardHeight)
 const cols = Math.floor(board.clientWidth / boardWidth)
 
+
 const blocks = []
+console.log(blocks);
+
+const snake = [{
+    x: 1,
+    y: 3
+}, {
+    x: 1,
+    y: 4
+}, {
+    x: 1,
+    y: 5
+}]
 
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
@@ -15,3 +28,12 @@ for (let row = 0; row < rows; row++) {
         block.innerText = `${row},${col}`
     }
 }
+
+
+function render() {
+    snake.forEach(function (elem) {
+        blocks[`${elem.x} - ${elem.y} `].classList.add("fill")
+    })
+}
+
+render()
