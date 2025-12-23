ac2a7bcf485ff5dc2ae09faaf930225db11be749
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-23 22:39:50 +0530

    jssnakeproject

--- snake-game/script.js
+++ snake-game/script.js
@@ -1,15 +1,17 @@
 const board = document.querySelector(".board")
-const boardHeight = 30
-const boardWidth = 30
+const boardHeight = 80
+const boardWidth = 80
 
 const rows = Math.floor(board.clientHeight / boardHeight)
 const cols = Math.floor(board.clientWidth / boardWidth)
-// console.log(rows);
-// console.log(cols);
 
-for(let i=0;i<=rows*cols;i++){
-    const block = document.createElement("div")
-    block.classList.add("block")
-    board.appendChild(block)
+
+for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols ; col++) {
+        const block = document.createElement("div")
+        block.classList.add("block")
+        board.appendChild(block)
+        block.innerText = `${row},${col}`
+        blocks
+    }
 }
-
