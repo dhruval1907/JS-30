c4941f6d3717cec5c96a24918cf507999e70fdb8
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-23 23:06:49 +0530

    jssnakeproject

--- snake-game/script.js
+++ snake-game/script.js
@@ -1,39 +1,38 @@
-const board = document.querySelector(".board")
-const boardHeight = 80
-const boardWidth = 80
+const board = document.querySelector(".board");
 
-const rows = Math.floor(board.clientHeight / boardHeight)
-const cols = Math.floor(board.clientWidth / boardWidth)
+const blockSize = 80;
+const rows = Math.floor(board.clientHeight / blockSize);
+const cols = Math.floor(board.clientWidth / blockSize);
 
+const blocks = [];
 
-const blocks = []
-console.log(blocks);
-
-const snake = [{
-    x: 1,
-    y: 3
-}, {
-    x: 1,
-    y: 4
-}, {
-    x: 1,
-    y: 5
-}]
+const snake = [
+    { x: 1, y: 3 },
+    { x: 1, y: 4 },
+    { x: 1, y: 5 }
+];
 
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
-        const block = document.createElement("div")
-        block.classList.add("block")
-        board.appendChild(block)
-        block.innerText = `${row},${col}`
+        const block = document.createElement("div");
+        block.classList.add("block");
+        block.innerText = `${row},${col}`;
+        board.appendChild(block);
+        blocks.push(block);
     }
 }
-
-
 function render() {
-    snake.forEach(function (elem) {
-        blocks[`${elem.x} - ${elem.y} `].classList.add("fill")
-    })
+    blocks.forEach(block => block.classList.remove("fill"));
+    snake.forEach(segment => {
+        const index = segment.x * cols + segment.y;
+        if (blocks[index]) {
+            blocks[index].classList.add("fill");
+        }
+    });
 }
 
-render()
+render();
+
+setInterval(() => { 
+    render()
+}, 300);
